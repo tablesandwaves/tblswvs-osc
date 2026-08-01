876f6f2d4b7e5a8e54bdc819c81d8917493a7c97
Steve Meyer <steve.e.meyer@gmail.com>
2026-08-01 11:19:36 -0500

    Default Arc mode: endless encoder

--- src/arc.ts
+++ src/arc.ts
@@ -10,19 +10,6 @@ export const ARC_ENCODER_STEP_COUNT     = 48;
 export const ARC_ENCODER_MIN_VALUE      = 0;
 export const ARC_ENCODER_MAX_VALUE      = ARC_ENCODER_STEP_COUNT * ARC_ENCODER_RESOLUTION;
 
-
-interface ArcDelta {
-  n: 0 | 1 | 2 | 3;
-  d: number;
-}
-
-
-interface ArcKeyPress {
-  n: 0 | 1 | 2 | 3;
-  s: 0 | 1;
-}
-
-
 const ARC_OSC_RECEIVER_PORT  = 12004;
 
 
@@ -41,18 +28,18 @@ export class Arc extends MonomeDevice {
     super.oscReceiver.removeAllListeners(super.prefix + "/enc/key");
     super.oscReceiver.removeAllListeners(super.prefix + "/enc/delta");
 
-    super.oscReceiver.on(super.prefix + "/enc/key", (index: number, state: number) => {
-      this.emit("key", { index: index, state: state });
+    super.oscReceiver.on(super.prefix + "/enc/key", (_: number, state: number) => {
+      this.emit("key", state);
     });
 
     super.oscReceiver.on(super.prefix + "/enc/delta", (index: number, delta: number) => {
-      this.emit("delta", { index: index, delta: delta });
+      this.#delta(index, delta);
     });
   }
 
 
   get encoderValues() {
-    return this.#encoderValues;
+    return this.#encoderValues.map(value => value / ARC_ENCODER_MAX_VALUE);
   }
 
 
@@ -60,21 +47,20 @@ export class Arc extends MonomeDevice {
   }
 
 
-  delta(delta: ArcDelta) {
-    // if ((this.#encoderValues[delta.n] == ARC_ENCODER_MIN_VALUE && delta.d < 0) ||
-    //     (this.#encoderValues[delta.n] == ARC_ENCODER_MAX_VALUE && delta.d > 0)) return;
+  #delta(index: number, delta: number) {
+    if ((this.#encoderValues[index] == ARC_ENCODER_MIN_VALUE && delta < 0) ||
+        (this.#encoderValues[index] == ARC_ENCODER_MAX_VALUE && delta > 0)) return;
 
-    // const previousValue = this.#encoderValues[delta.n];
-    // this.#encoderValues[delta.n] += delta.d;
-    // this.#encoderValues[delta.n] = this.#encoderValues[delta.n] < ARC_ENCODER_MIN_VALUE ?
-    //     ARC_ENCODER_MIN_VALUE : this.#encoderValues[delta.n];
-    // this.#encoderValues[delta.n] = this.#encoderValues[delta.n] > ARC_ENCODER_MAX_VALUE ?
-    //     ARC_ENCODER_MAX_VALUE : this.#encoderValues[delta.n];
+    const previousValue = this.#encoderValues[index];
+    this.#encoderValues[index] += delta;
+    this.#encoderValues[index] = this.#encoderValues[index] < ARC_ENCODER_MIN_VALUE ?
+        ARC_ENCODER_MIN_VALUE : this.#encoderValues[index];
+    this.#encoderValues[index] = this.#encoderValues[index] > ARC_ENCODER_MAX_VALUE ?
+        ARC_ENCODER_MAX_VALUE : this.#encoderValues[index];
 
-    // // Tell the main application the updated value in normalized 0-1 range to pass onto the current track.
-    // this.emit("update-parameter", delta.n, this.#encoderValues[delta.n] / ARC_ENCODER_MAX_VALUE);
-
-    // this.#updateDeviceDials(delta.n, previousValue, this.#encoderValues[delta.n]);
+    // Tell listeners the new value normalized to 0-1 range, then update dial values.
+    this.emit("parameter", { index: index, value: this.#encoderValues[index] / ARC_ENCODER_MAX_VALUE });
+    this.#updateDeviceDials(index, previousValue, this.#encoderValues[index]);
   }
 
 
@@ -91,26 +77,24 @@ export class Arc extends MonomeDevice {
 
 
   #updateDeviceDials(dialIndex: number, previousValue: number, newValue: number) {
-    // const previousLedValue   = previousValue / ARC_ENCODER_RESOLUTION;
-    // const ledValue           = this.#encoderValues[dialIndex] / ARC_ENCODER_RESOLUTION;
-    // const updatePartialValue = (ledValue % 1) * ARC_ENCODER_RESOLUTION;
+    const previousLedValue   = previousValue / ARC_ENCODER_RESOLUTION;
+    const ledValue           = this.#encoderValues[dialIndex] / ARC_ENCODER_RESOLUTION;
+    const updatePartialValue = (ledValue % 1) * ARC_ENCODER_RESOLUTION;
 
-    // if (newValue > previousValue) {
-    //   this.#device.range(
-    //     dialIndex,
-    //     Math.floor(previousLedValue - 1) + ARC_ENCODER_OFFSET,
-    //     (Math.floor(ledValue) + ARC_ENCODER_OFFSET) % ARC_ENCODER_LED_COUNT,
-    //     ARC_ENCODER_MAX_BRIGHTNESS
-    //   );
-    //   this.#device.set(dialIndex, (Math.floor(ledValue) + 41) % ARC_ENCODER_LED_COUNT, updatePartialValue);
-    // } else {
-    //   this.#device.range(
-    //     dialIndex,
-    //     (Math.floor(ledValue) + 39) % ARC_ENCODER_LED_COUNT,
-    //     (Math.floor(previousLedValue) + 41) % ARC_ENCODER_LED_COUNT,
-    //     ARC_ENCODER_MIN_BRIGHTNESS
-    //   );
-    //   this.#device.set(dialIndex, (Math.floor(previousLedValue) + 40) % ARC_ENCODER_LED_COUNT - 1, updatePartialValue);
-    // }
+    if (newValue > previousValue) {
+      super.oscSender.send(super.prefix + "/ring/range",
+        { type: "integer", value: dialIndex },
+        { type: "integer", value: Math.floor(previousLedValue - 1) + ARC_ENCODER_OFFSET },
+        { type: "integer", value: (Math.floor(ledValue) + ARC_ENCODER_OFFSET) % ARC_ENCODER_LED_COUNT },
+        { type: "integer", value: ARC_ENCODER_MAX_BRIGHTNESS },
+      );
+    } else {
+      super.oscSender.send(super.prefix + "/ring/range",
+        { type: "integer", value: dialIndex },
+        { type: "integer", value: (Math.floor(ledValue) + 39) % ARC_ENCODER_LED_COUNT },
+        { type: "integer", value: (Math.floor(previousLedValue) + 41) % ARC_ENCODER_LED_COUNT },
+        { type: "integer", value: ARC_ENCODER_MIN_BRIGHTNESS },
+      );
+    }
   }
 }
