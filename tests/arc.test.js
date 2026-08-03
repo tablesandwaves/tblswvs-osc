6be23539d382faaa323c499cc440f8562319f13c
Steve Meyer <steve.e.meyer@gmail.com>
2026-08-02 19:53:26 -0500

    Arc tests for clear encoder values/display

--- tests/arc.test.js
+++ tests/arc.test.js
@@ -110,4 +110,31 @@ describe("Arc", () => {
       arc.setDialValues([0.5, 0.5, 1, 1.1]);
     }, /^Error: encoder values must be between 0 and 1/);
   });
+
+  it("can clear all encoder/parameter values", (_, done) => {
+    const messages = { ringAllLedResets: 0, ringRangeSets: 0 };
+
+    const checkFinished = () => {
+      if (messages.ringAllLedResets === 4 && messages.ringRangeSets === 4) {
+        try {
+          assert.deepEqual(arc.encoderValues, [0, 0, 0, 0]);
+          done();
+        } catch (error) {
+          done(error);
+        }
+      }
+    }
+
+    receiver.on("/monome/ring/all", (...args) => {
+      messages.ringAllLedResets += 1;
+      checkFinished();
+    });
+
+    receiver.on("/monome/ring/range", (...args) => {
+      messages.ringRangeSets += 1;
+      checkFinished();
+    });
+
+    arc.clearDisplay();
+  });
 });
